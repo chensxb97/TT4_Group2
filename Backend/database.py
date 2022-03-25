a603f1824ddbb7900f1bcf239f85f9c5b454b24b
Deepak Joshi <1800546@sit.singaporetech.edu.sg>
2022-03-25 16:31:10 +0800

    added login check

--- Backend/database.py
+++ Backend/database.py
@@ -1,7 +1,21 @@
 import sys
 import mysql.connector
+import configparser
 
 main_list = sys.argv[1:]
+local_db_file = "local_db.txt"
+config = configparser.ConfigParser()
+config.read(local_db_file)
+
+"""
+login
+signup
+balance
+currentloan
+loanhistory
+newloan
+payloan
+"""
 
 mydb = mysql.connector.connect(
   host="127.0.0.1",
@@ -11,11 +25,47 @@ mydb = mysql.connector.connect(
   port=3306,
 )
 
+if main_list[0] == "login":
+
+    try:
+        login_username = main_list[1]
+        login_password = main_list[2]
+        login_password_actual = config.get('user', login_username)
+        if login_password == login_password_actual:
+            print("True")
+        else:
+            print("False")
+    except:
+        print("False")
+
+if main_list[0] == "signup":
+    signup_username = main_list[1]
+    signup_password = main_list[2]
+    signup_name = main_list[3]
+    signup_phone = main_list[4]
+    signup_address = main_list[5]
+
+if main_list[0] == "balance":
+    balance_username = main_list[1]
+
+if main_list[0] == "currentloan":
+    currentloan_username = main_list[1]
+
+if main_list[0] == "loanhistory":
+    loanhistory_username = main_list[1]
+
+if main_list[0] == "newloan":
+    newloan_username = main_list[1]
+    newloan_id = main_list[2]
+    newloan_amount = main_list[3]
+
+if main_list[0] == "payloan":
+    payloan_username = main_list[1]
+    payloan_id = main_list[2]
+    payloan_amount = main_list[3]
+
 mycursor = mydb.cursor()
-
 mycursor.execute("SELECT * FROM customer")
-
 myresult = mycursor.fetchall()
-
 for x in myresult:
   print(x)
