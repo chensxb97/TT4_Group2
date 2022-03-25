80fc38a2e8b5d5512d51a000159e5f4811beafd9
Yu Xiang <sylomrouza24@gmail.com>
2022-03-25 14:14:03 +0800

    Initial Prototype for Landing, Login and Register webpages

--- templates/login/Login.js
+++ templates/login/Login.js
@@ -1,11 +1,29 @@
-function myFunction(){
-    const email = document.getElementById("login_email").value
-    const password = document.getElementById("login_password").value
+function userLogin() {
 
-    if(email==="test@abc.com" && password==="123123"){
-        alert("Success, directing you to home page");
-        document.location.href = "../home/Home.html"
-    }else{
-        alert("Invalid credentials");
-    }
+    // const email = document.getElementById("login_email").value
+    // const password = document.getElementById("login_password").value
+    //
+    // $.ajax({
+    //     type: "GET",
+    //     url: "check",
+    //     data: {
+    //         "myvalue": email.concat(",", password),
+    //     },
+    //     dataType: "json"
+    // }).done(function (json) {
+    //     $.each(json, function (key, val) {
+    //         if (val === "Success") {
+    //             alert("Login Successful, directing you to home page");
+    //             document.location.href = "../home/Home.html"
+    //         }
+    //     });
+    // });
+
+    // if(email==="test@abc.com" && password==="123123"){
+    //     alert("Success, directing you to home page");
+    //     document.location.href = "../home/Home.html"
+    // }else{
+    //     alert("Invalid credentials");
+    // }
 }
+
