bf765a1df1181188873085e005859c1460cc961f
Yu Xiang <sylomrouza24@gmail.com>
2022-03-25 14:42:20 +0800

    Update template folder structure

--- templates/login/Login.js
+++ templates/login/Login.js
@@ -1,35 +1,29 @@
-// document.getElementById(id_name)
-// document.getElementsByClassName('class_name')
-// document.querySelector('selector')
+function userLogin() {
 
-
-// const loginForm = document.getElementById("login-form");
-// const loginButton = document.getElementById("login-form-submit");
-// const loginErrorMsg = document.getElementById("login-error-msg");
-//
-// loginButton.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     const username = loginForm.username.value;
-//     const password = loginForm.password.value;
-//
-//     if (username === "user" && password === "web_dev") {
-//         alert("You have successfully logged in.");
-//         location.reload();
-//     } else {
-//         loginErrorMsg.style.opacity = 1;
-//     }
-// })
-
-
-function myFunction(){
     const email = document.getElementById("login_email").value
     const password = document.getElementById("login_password").value
 
-    if(email==="test@abc.com" && password==="123123"){
-        alert("Success, directing you to home page");
-        document.location.href = "../home/Home.html"
-    }else{
-        alert("Invalid credentials");
-    }
+    $.ajax({
+        type: "GET",
+        url: "check",
+        data: {
+            "myvalue": email.concat(",", password),
+        },
+        dataType: "json"
+    }).done(function (json) {
+        $.each(json, function (key, val) {
+            if (val === "Success") {
+                alert("Login Successful, directing you to home page");
+                document.location.href = "../home/Home.html"
+            }
+        });
+    });
 
+    // if(email==="test@abc.com" && password==="123123"){
+    //     alert("Success, directing you to home page");
+    //     document.location.href = "../home/Home.html"
+    // }else{
+    //     alert("Invalid credentials");
+    // }
 }
+
